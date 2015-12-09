050a1e0ce4b0ddeac476495452daa70bf4772d7a
hyj143 <hyj143@gmail.com>
2015-12-08 19:16:55 -0500

    call random walk model

--- DWM_WF_Wake.F90
+++ DWM_WF_Wake.F90
@@ -101,6 +101,7 @@ SUBROUTINE calculate_DWM ( blade_radius, num_element, r_t  )
     IF (ranW==0)   THEN     
         CALL Get_wake_center ( wake_width, wake_position )
     ELSE IF (ranW == 1)   THEN
+        CALL Get_wake_center_RW (wake_position)
     END IF
     
          !OPEN(unit = 10, status='replace',file='DWM\results\meandered_wake_center.bin',form='unformatted')    
